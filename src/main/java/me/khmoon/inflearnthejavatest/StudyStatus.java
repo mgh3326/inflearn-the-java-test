92d54bba58374b0a72ab8a90bcfeb84b5a76511c
robin <mgh3326@naver.com>
2019-12-21 20:27:41 +0900

    JUnit 5 조건에 따라 테스트 실행하기

---
+++ src/main/java/me/khmoon/inflearnthejavatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package me.khmoon.inflearnthejavatest;
+
+public enum StudyStatus {
+  DRAFT, STARTED, ENDED
+}
--- src/test/java/me/khmoon/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/khmoon/inflearnthejavatest/StudyTest.java
@@ -2,36 +2,40 @@ package me.khmoon.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
-  @Test
-  void create() {
-    Study study = new Study();
-    assertNotNull(study);
-    System.out.println("create");
-  }
-  @Test
-  @Disabled
-  void create1() {
-    Study study = new Study();
-    assertNotNull(study);
-    System.out.println("create1");
-  }
   @BeforeAll
-  static void beforeAll(){
+  static void beforeAll() {
     System.out.println("before all");
   }
+
   @AfterAll
-  static void afterAll(){
+  static void afterAll() {
     System.out.println("after all");
   }
-  @BeforeEach
-  void beforeEach(){
-    System.out.println("before each");
+
+  @Test
+  @DisplayName("스터디 만들기 ╯°□°）╯")
+  void create_new_study() {
+    Study actual = new Study(10);
+    assertThat(actual.getLimit()).isGreaterThan(0);
   }
+
+  @Test
+  @DisplayName("스터디 만들기 \uD83D\uDE31")
+  void create_new_study_again() {
+    System.out.println("create1");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("Before each");
+  }
+
   @AfterEach
-  void afterEach(){
-    System.out.println("after each");
+  void afterEach() {
+    System.out.println("After each");
   }
 }
